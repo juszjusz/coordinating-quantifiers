d983731f5d9110327d778fef0289bd31f41683ea
darkal85 <36086663+darkal85@users.noreply.github.com>
2019-06-07 14:32:33 +0200

    simulation etc

--- objects/agent.py
+++ objects/agent.py
@@ -2,8 +2,24 @@ from objects.language import Language
 import random
 
 class Population:
+
+    agents = []
+    pair = 0
+
     def __init__(self, population_size: int):
         self.population = []
+        for i in range(population_size):
+            self.population.append(Agent(i))
+
+
+    def select_pairs(self):
+        self.agents = random.sample(range(self.parameters["populationSize"]), self.parameters["gamesPerRound"] * 2)
+        pair = 0
+
+    def get_speaker_and_hearer(self):
+        speaker_and_hearer = self.agents[2*self.pair]._as_speaker(), self.agents[2*self.pair+1]._as_hearer()
+        self.pair = self.pair + 1
+        return speaker_and_hearer
 
 
 class Agent:
