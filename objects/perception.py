7b5739b141cb7d08a9e0256eeec06c02c4995cef
darkal85 <36086663+darkal85@users.noreply.github.com>
2019-06-26 18:37:48 +0200

    Update perception.py

--- objects/perception.py
+++ objects/perception.py
@@ -1,30 +1,52 @@
 import random
 import numpy as np
-# import matplotlib.pyplot as plt
-
+import itertools
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
 
 class Stimulus:
     # stimulus represents a perceptual situation where two exact quantities are given
 
-    def __init__(self):
-        self.a = random.randint(0, 101)  # ?
-        self.b = random.randint(1, 101)  # ?
+    def __init__(self, a=None, b=None):
+        if a is None:
+            self.a = random.randint(0, 101)  # ?
+            self.b = random.randint(1, 101)  # ?
+        else:
+            self.a = a
+            self.b = b
 
 
 class ReactiveUnit:
 
     sigma = 0.1
-    sample_size = 1000
+    sample_size = 10000
 
     def __init__(self, stimulus: Stimulus):
-        representation1 = self.get_representation(stimulus.a)
-        representation2 = self.get_representation(stimulus.b)
+        a_samples = self.sample(stimulus.a)
+        b_samples = self.sample(stimulus.b)
+        self.a = stimulus.a
+        self.b = stimulus.b
         # print(stimulus.a/stimulus.b)
-        self.ratios = representation1/representation2
+        r = a_samples/b_samples
+        self.ratio_samples = r.tolist()
 
-    def get_representation(self, quantity):
-        return np.array(np.random.normal(quantity, self.sigma, self.sample_size), dtype=np.float)
+    def sample(self, quantity):
+        return np.array(np.random.normal(quantity, self.sigma*quantity, self.sample_size), dtype=np.float)
 
+    def show(self, how="spline"):
+        plt.title("Reactive unit for " + str(self.a) + "/" + str(self.b) + " = " + str(self.a / self.b))
+        if how == "hist":
+            plt.hist(self.ratio_samples, bins="auto")
+            plt.show()
+        elif how == "spline":
+            y, bin_edges = np.histogram(self.ratio_samples, bins=45)
+            # x = np.arange((bin_edges[0] + bin_edges[1])/2, bin_edges[-1], bin_edges[1] - bin_edges[0])
+            x = [(bin_edges[i] + bin_edges[i+1])/2 for i in range(0, len(bin_edges)-1)]
+            f = interp1d(x, y)
+            f2 = interp1d(x, y, kind="cubic")
+            plt.plot(x, y, 'o', x, f(x), '-', x, f2(x), '--')
+            plt.legend(['data', 'linear', 'cubic'], loc='best')
+            plt.show()
 
 class DiscriminativeCategory:
 
@@ -37,12 +59,15 @@ class DiscriminativeCategory:
             self.reactive_units = reactive_units
 
     def response(self, stimulus: Stimulus):
-        ru = ReactiveUnit(stimulus)
-        hist, bin_edges = np.histogram(ru.ratios, bins="auto")
-        bin_width = bin_edges[1] - bin_edges[0]
-        # plt.hist(ru.ratios, bins="auto")
-        # plt.title("Representation of stimulus")
-        # plt.show()
+        #ru = ReactiveUnit(stimulus)
+        #total_ratios = ru.ratios + self.get_all_ratios()
+        #total_hist, total_bin_edges = np.histogram(total_ratios, bins="auto")
+        #bin_width = total_bin_edges[1] - total_bin_edges[0]
+        #print(stimulus.a, stimulus.b)
+        #plt.hist(ru.ratios, bins=[i for i in np.arange(1,3,0.007)])
+        #plt.title("Representation of stimulus")
+        #plt.show()
+        pass
 
     def add_reactive_unit(self, reactive_unit: ReactiveUnit, weight=0.5):
         self.weights.append(weight)
@@ -53,3 +78,17 @@ class DiscriminativeCategory:
 
     def update_weights(self):
         pass
+
+    def show(self):
+        flat = self.get_flat()
+        plt.hist(flat, bins="auto")
+        plt.title("category")
+        plt.show()
+
+    def get_flat(self):
+        flat_ratios = []
+        flat_weights = []
+        for i in range(0, len(self.reactive_units)):
+            flat_ratios += self.reactive_units[i].ratios
+            flat_weights += [self.weights[i]]*len(self.reactive_units[i].ratios)
+        return flat_ratios, flat_weights
