bc9d797309ba8ec046a9cb83aa11c59ecdd332e0
darkal85 <36086663+darkal85@users.noreply.github.com>
2019-06-27 21:15:55 +0200

    response of category
    response method - first working version

--- objects/perception.py
+++ objects/perception.py
@@ -1,7 +1,6 @@
 from random import randint
 import scipy.integrate
 import numpy as np
-import itertools
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 
@@ -16,19 +15,32 @@ class Stimulus:
 
 class ReactiveUnit:
     sigma = 0.1
-    sample_size = 100000
+    sample_size = 10000
 
     def __init__(self, stimulus: Stimulus):
         a_samples = self.sample(stimulus.a)
         b_samples = self.sample(stimulus.b)
         self.a = stimulus.a
         self.b = stimulus.b
-        # print(stimulus.a/stimulus.b)
         r = a_samples / b_samples
         self.ratio_samples = r.tolist()
+        y, bin_edges = np.histogram(self.ratio_samples, bins=50)
+        x = [(bin_edges[i] + bin_edges[i + 1]) / 2 for i in range(0, len(bin_edges) - 1)]
+        self.interp = interp1d(x, y, fill_value="extrapolate") #radial basis interpolation?
+        self.x_left = x[0]
+        self.x_right = x[-1]
 
     def sample(self, quantity):
         return np.array(np.random.normal(quantity, self.sigma * quantity, self.sample_size), dtype=np.float)
+
+    def fun(self, x):
+        if type(x) is np.ndarray:
+            y = np.empty_like(x)
+            for i in range(len(x)):
+                y[i] = 0 if x[i] < self.x_left or x[i] > self.x_right else self.interp(x[i])
+            return y
+        else:
+            return 0 if x < self.x_left or x > self.x_right else self.interp(x)
 
     def show(self, how="spline"):
         plt.title("Reactive unit for " + str(self.a) + "/" + str(self.b) + " = " + str(self.a / self.b))
@@ -36,71 +48,55 @@ class ReactiveUnit:
             plt.hist(self.ratio_samples, bins="auto")
             plt.show()
         elif how == "spline":
-            y, bin_edges = np.histogram(self.ratio_samples, bins=50)
-            # x = np.arange((bin_edges[0] + bin_edges[1])/2, bin_edges[-1], bin_edges[1] - bin_edges[0])
-            x = [(bin_edges[i] + bin_edges[i + 1]) / 2 for i in range(0, len(bin_edges) - 1)]
-            f = interp1d(x, y)
-            f2 = interp1d(x, y, kind="cubic")
-            result = scipy.integrate.quad(lambda x: 0.5 * f2(x)*f2(x), 0.41, 1.19)
-            print(result)
-            plt.plot(x, y, 'o', x, f(x), '-', x, f2(x), '--')
-            plt.legend(['data', 'linear', 'cubic'], loc='best')
+            x = np.linspace(self.x_left, self.x_right)
+            plt.plot(x, self.fun(x), 'o', x, self.fun(x), '--')
+            plt.legend(['data', 'cubic'], loc='best')
             plt.hist(self.ratio_samples, bins=50)
             plt.show()
 
 
 class DiscriminativeCategory:
 
-    def __init__(self, weights=[], reactive_units=[]):
-        self.weights = weights
-        self.reactive_units = reactive_units
-
+    def __init__(self):
+        self.weights = []
+        self.reactive_units = []
+        self.x_left = float("inf")
+        self.x_right = float("-inf")
 
     def response(self, stimulus: Stimulus):
-        # ru = ReactiveUnit(stimulus)
-        # total_ratios = ru.ratios + self.get_all_ratios()
-        # total_hist, total_bin_edges = np.histogram(total_ratios, bins="auto")
-        # bin_width = total_bin_edges[1] - total_bin_edges[0]
-        # print(stimulus.a, stimulus.b)
-        # plt.hist(ru.ratios, bins=[i for i in np.arange(1,3,0.007)])
-        # plt.title("Representation of stimulus")
-        # plt.show()
-        pass
-
+        s = ReactiveUnit(stimulus)
+        x_left = min(s.x_left, self.x_left)
+        x_right = max(s.x_right, self.x_right)
+        return scipy.integrate.quad(lambda x: s.fun(x)*self.fun(x), x_left, x_right)[0]
 
     def add_reactive_unit(self, reactive_unit: ReactiveUnit, weight=0.5):
         self.weights.append(weight)
         self.reactive_units.append(reactive_unit)
+        self.x_left = min(self.x_left, reactive_unit.x_left)
+        self.x_right = max(self.x_right, reactive_unit.x_right)
 
+    def fun(self, x):
+        return 0 if len(self.reactive_units) == 0 \
+            else sum([r.fun(x)*w for r, w in zip(self.reactive_units, self.weights)])
 
     def select(self, stimuli):
         return 0
 
-
     def update_weights(self):
         pass
 
-
     def show(self):
-        flat = self.get_flat()
-        plt.hist(flat, bins="auto")
-        plt.title("category")
+        x = np.linspace(self.x_left, self.x_right, num=100)
+        plt.plot(x, self.fun(x), 'o', x, self.fun(x), '--')
+        plt.legend(['data', 'cubic'], loc='best')
+        # plt.hist(self.ratio_samples, bins=50)
         plt.show()
 
-
     def get_flat(self):
-        flat_ratios = []
-        flat_weights = []
-        for i in range(0, len(self.reactive_units)):
-            flat_ratios += self.reactive_units[i].ratios
-            flat_weights += [self.weights[i]] * len(self.reactive_units[i].ratios)
-        return flat_ratios, flat_weights
-
-
-#ru = ReactiveUnit(Stimulus(3,4))
-#ru2 = ReactiveUnit(Stimulus())
-#ru.show()
-
-#c = DiscriminativeCategory()
-#c.add_reactive_unit(ru, 0.3)
-#c.add_reactive_unit(ru2, 0.6)
+        #flat_ratios = []
+        #flat_weights = []
+        #for i in range(0, len(self.reactive_units)):
+        #    flat_ratios += self.reactive_units[i].ratios
+        #    flat_weights += [self.weights[i]] * len(self.reactive_units[i].ratios)
+        #return flat_ratios, flat_weights
+        return []
