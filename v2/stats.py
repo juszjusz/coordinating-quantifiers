6dacd372517296febd84efa4903ae9462fdd545c
juszynski <uszynskijakub@gmail.com>
2023-09-13 18:21:33 +0200

    active lexicon plot

--- v2/stats.py
+++ v2/stats.py
@@ -1,3 +1,5 @@
+from typing import List
+import numpy as np
 from numpy import mean
 from numpy import std
 from scipy import stats
@@ -18,6 +20,29 @@ def confidence_interval(sample, level=0.95, method='t'):
     return lower_bound, upper_bound
 
 
+def new_confidence_intervals(sample_series: np.ndarray, level=0.95, method='t'):
+    shape = sample_series.shape
+    assert len(shape) == 2, 'function computes confidence intervals for list of sequences'
+    n, sequence_length = shape
+
+    means = np.mean(sample_series, axis=0)
+    stdevs = np.std(sample_series, axis=0)
+
+    if method == 't':
+        test_stat = stats.t.ppf((level + 1) / 2, n)
+    elif method == 'z':
+        test_stat = stats.norm.ppf((level + 1) / 2)
+    else:
+        raise NotImplementedError(f'{method} is an unknown method for computing CI, implemented for "t" and "z"')
+
+    margin = (test_stat * stdevs) / sqrt(n)
+
+    lower_bound = means - margin
+    upper_bound = means + margin
+
+    return lower_bound, upper_bound
+
+
 def means(sample_series):
     return [mean(sample_series[t]) for t in range(len(sample_series))]
 
@@ -27,3 +52,29 @@ def confidence_intervals(sample_series):
     cis_l = [i[0] for i in cis]
     cis_u = [i[1] for i in cis]
     return cis_l, cis_u
+
+
+if __name__ == '__main__':
+    a = np.array([[0, 4, 6, 6, 6, 6, 6, 6, 6, 6, 7, 8, 9],
+                  [0, 4, 5, 5, 6, 6, 6, 6, 7, 7, 7, 9, 11],
+                  [0, 4, 4, 4, 4, 5, 5, 6, 6, 6, 6, 8, 8],
+                  [0, 5, 5, 5, 6, 7, 7, 7, 7, 7, 7, 8, 10],
+                  [0, 4, 6, 6, 6, 5, 5, 5, 5, 5, 6, 7, 10],
+                  [0, 4, 4, 5, 5, 5, 5, 6, 5, 6, 6, 6, 7],
+                  [0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 7],
+                  [0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 8],
+                  [0, 5, 5, 4, 4, 3, 4, 4, 6, 7, 8, 10, 10],
+                  [0, 6, 6, 5, 5, 5, 5, 5, 6, 7, 7, 8, 9],
+                  [0, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 8],
+                  [0, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 7, 8],
+                  [0, 5, 5, 7, 6, 7, 7, 7, 7, 7, 7, 9, 10],
+                  [0, 4, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 9],
+                  [0, 5, 5, 5, 5, 6, 7, 7, 7, 7, 7, 7, 9],
+                  [0, 4, 4, 4, 4, 4, 4, 4, 4, 5, 6, 7, 10],
+                  [0, 4, 4, 4, 5, 5, 5, 5, 5, 7, 7, 8, 8],
+                  [0, 5, 4, 5, 5, 5, 5, 7, 7, 7, 7, 9, 10],
+                  [0, 5, 4, 4, 5, 5, 5, 5, 5, 5, 6, 7, 10],
+                  [0, 5, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 6]])
+    # x = new_confidence_intervals(a)
+    x = new_confidence_intervals(a)
+    print(x)
